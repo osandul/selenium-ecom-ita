cd010ecfed6fd4132114145421ae5a8c6de88d01
Oleksandra_Sandul <sashka95.13@gmail.com>
2021-11-11 12:16:30 +0200

    fixes for new website under test

--- src/main/java/driver/DriverInit.java
+++ src/main/java/driver/DriverInit.java
@@ -1,34 +1,34 @@
 package driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
 
 public class DriverInit {
 
     public static WebDriver driver;
 
-    public static void setDriver(WebDriver webDriver) {
-        driver = webDriver;
-        driver.get("http://automationpractice.com/");
-    }
-
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            try {
-                setDriver(new ChromeDriver());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            setDriver();
         }
         return driver;
     }
 
-    public static void closeDriver() {
-        if (driver != null) {
-            getDriver().quit();
-            driver = null;
-        }
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.opencart.com");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
     }
+
+    public void closeDriver() {
+        getDriver().quit();
+    }
+
 }
---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import driver.DriverInit;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.assertj.core.api.Assertions;
+
+public class LoginTest {
+
+    WebDriver driver = DriverInit.getDriver();
+    public String expectedText = "Warning: No match for E-Mail Address and/or Password.";
+
+    public WebElement waitForVisibilityOfElement(WebElement locator) {
+        return new WebDriverWait(driver, 200).until(ExpectedConditions.visibilityOf(locator));
+    }
+
+    @Test
+    public void loginTest() {
+        waitForVisibilityOfElement(driver.findElement(By.linkText("My Account"))).click();
+        waitForVisibilityOfElement(driver.findElement(By.xpath("//li[contains(@class, 'dropdown')]//following::a[text()='Login']"))).click();
+        waitForVisibilityOfElement(driver.findElement(By.id("input-email"))).sendKeys("test@gmail.com");
+        waitForVisibilityOfElement(driver.findElement(By.name("password"))).sendKeys("1234");
+        waitForVisibilityOfElement(driver.findElement(By.xpath("//input[@type='submit']"))).click();
+
+        String actualError = waitForVisibilityOfElement(driver
+                .findElement(By.xpath("//div[contains(@class, 'alert')]"))).getText();
+
+        Assertions.assertThat(actualError).as("error message is different").isEqualTo(expectedText);
+
+    }
+}
